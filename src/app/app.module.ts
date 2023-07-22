331ddd1e8a803e3baeaef2933acc45ab17304e20
Sumit Sangale <sumit@thinkoverit.com>
2023-07-22 13:06:42 +0530

    create signup templete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import { HeaderComponent } from "./header/header.component";
 import { PostListComponent } from "./posts/post-list/post-list.component";
 import { AppRoutingModule } from "./app-routing.module";
 import { loginComponent } from "./auth/login/login.component";
+import { SignupComponent } from "./auth/signup/signup.component";
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { loginComponent } from "./auth/login/login.component";
     PostCreateComponent,
     HeaderComponent,
     PostListComponent,
-    loginComponent
+    loginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
