5d42de452c815317dd8979699d2f9c7b311704ca
Sumit Sangale <sumit@thinkoverit.com>
2023-07-22 23:51:19 +0530

    authgaurd implemented on UI to protect route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,17 +5,19 @@ import { PostListComponent } from "./posts/post-list/post-list.component";
 import { PostCreateComponent } from "./posts/post-create/post-create.component";
 import { loginComponent } from "./auth/login/login.component";
 import { SignupComponent } from "./auth/signup/signup.component";
+import { AuthGaurd } from "./auth/auth.gaurd";
 
 const routes: Routes = [
   { path: '', component: PostListComponent },
-  { path: 'create', component: PostCreateComponent },
-  { path: 'edit/:postId', component: PostCreateComponent },
+  { path: 'create', component: PostCreateComponent, canActivate: [AuthGaurd] },
+  { path: 'edit/:postId', component: PostCreateComponent, canActivate: [AuthGaurd] },
   { path: 'login', component: loginComponent },
   { path: 'signup', component: SignupComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGaurd]
 })
 export class AppRoutingModule {}
